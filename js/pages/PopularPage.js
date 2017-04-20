eb124a2a8115a1dab173d90612a643f59371ac06
MoonGi6466 <617186658@qq.com>
2017-04-20 18:32:42 +0800

    完成了热门页面的ScrollableTabView

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -10,13 +10,23 @@ import {
 } from 'react-native';
 
 import NavigationBar from "../component/NavigationBar"
-// import ScrollableTabView from "react-native-scrollable-tab-view"
+import ScrollableTabView from "react-native-scrollable-tab-view"
 
 export default class PopularPage extends Component{
 
     render(){
         return <View style={styles.container}>
             <NavigationBar/>
+            <ScrollableTabView
+                tabBarBackgroundColor="#63B8FF"
+                tabBarActiveTextColor="#FFF"
+                tabBarInactiveTextColor="#F5FFFA"
+                tabBarUnderlineStyle={{backgroundColor:"#E7E7E7",height:2}}>
+                <Text tabLabel='IOS'/>
+                <Text tabLabel='Android'/>
+                <Text tabLabel='Java'/>
+                <Text tabLabel='JavaScript'/>
+            </ScrollableTabView>
         </View>;
     }
 }
